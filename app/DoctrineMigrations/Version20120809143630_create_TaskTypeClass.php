627d32176119ac6c167ecd4e572d4f827bcd1aab
Jon Cram <jon@webignition.net>
2012-08-09 14:37:52 +0100

    Added TaskTypeClass

---
+++ app/DoctrineMigrations/Version20120809143630_create_TaskTypeClass.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Application\Migrations;
+
+use SimplyTestable\BaseMigrationsBundle\Migration\BaseMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20120809143630_create_TaskTypeClass extends BaseMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->statements['mysql'] = array(
+            "CREATE TABLE TaskTypeClass (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description LONGTEXT DEFAULT NULL,
+                UNIQUE INDEX UNIQ_F92FE5F25E237E06 (name),
+                PRIMARY KEY(id)) ENGINE = InnoDB"
+        );
+        
+        $this->statements['sqlite'] = array(
+            "CREATE TABLE TaskTypeClass (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description LONGTEXT DEFAULT NULL)",
+            "CREATE UNIQUE INDEX UNIQ_F92FE5F25E237E06 ON TaskTypeClass (name)"
+        );
+        
+        parent::up($schema);
+    }
+   
+
+    public function down(Schema $schema)
+    {    
+        $this->addCommonStatement("DROP TABLE TaskTypeClass");      
+        
+        parent::down($schema);
+    }
+    
+}
