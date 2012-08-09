44a78b4ba1c05e929866df6a71a1391e28f3acba
Jon Cram <jon@webignition.net>
2012-08-09 18:21:30 +0100

    Added TaskTypeService

---
+++ src/SimplyTestable/WorkerBundle/Services/TaskTypeService.php
@@ -0,0 +1,38 @@
+<?php
+namespace SimplyTestable\WorkerBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use SimplyTestable\WorkerBundle\Entity\Task\Type;
+
+
+class TaskTypeService extends EntityService {
+    
+    const ENTITY_NAME = 'SimplyTestable\WorkerBundle\Entity\Task\Type';
+    
+    /**
+     *
+     * @return string
+     */
+    protected function getEntityName() {
+        return self::ENTITY_NAME;
+    }
+
+    
+    /**
+     * @param string $name
+     * @return \SimplyTestable\WorkerBundle\Entity\Task\Type
+     */
+    public function fetch($name) {        
+        return $this->getEntityRepository()->findOneByName($name);
+    }
+    
+    
+    /**
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public function has($name) {
+        return !is_null($this->fetch($name));
+    }   
+}
