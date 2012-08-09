9b0d20b30301d57351554b7024377f857b3a5a6f
Jon Cram <jon@webignition.net>
2012-08-09 14:41:11 +0100

    Added TaskType

---
+++ app/DoctrineMigrations/Version20120809143640_create_TaskType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Application\Migrations;
+
+use SimplyTestable\BaseMigrationsBundle\Migration\BaseMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20120809143640_create_TaskType extends BaseMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->statements['mysql'] = array(
+            "CREATE TABLE TaskType (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                tasktypeclass_id INT NOT NULL,
+                description LONGTEXT NOT NULL,
+                selectable TINYINT(1) NOT NULL,
+                UNIQUE INDEX UNIQ_F7737B3C5E237E06 (name),
+                INDEX IDX_F7737B3CAEA19A54 (tasktypeclass_id),
+                PRIMARY KEY(id)) ENGINE = InnoDB",
+            "ALTER TABLE TaskType ADD CONSTRAINT FK_F7737B3CAEA19A54 FOREIGN KEY (tasktypeclass_id) REFERENCES TaskTypeClass (id)"
+        );
+        
+        $this->statements['sqlite'] = array(
+            "CREATE TABLE TaskType (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                tasktypeclass_id INT NOT NULL,
+                description LONGTEXT NOT NULL,
+                selectable TINYINT(1) NOT NULL,
+                FOREIGN KEY(tasktypeclass_id) REFERENCES TaskTypeClass (id))",
+            "CREATE UNIQUE INDEX UNIQ_F7737B3C5E237E06 ON TaskType (name)",
+            "CREATE INDEX IDX_F7737B3CAEA19A54 ON TaskType (tasktypeclass_id)"
+        ); 
+        
+        parent::up($schema);
+    }
+   
+
+    public function down(Schema $schema)
+    {   
+        $this->statements['mysql'] = array(
+            "ALTER TABLE TaskType DROP FOREIGN KEY FK_F7737B3CAEA19A54"
+        );
+        
+        $this->addCommonStatement("DROP TABLE TaskType");      
+        
+        parent::down($schema);
+    }   
+}
---
+++ src/SimplyTestable/WorkerBundle/Entity/Task/Type/Type.php
@@ -0,0 +1,182 @@
+<?php
+namespace SimplyTestable\WorkerBundle\Entity\Task\Type;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\SerializerBundle\Annotation as SerializerAnnotation;
+
+/**
+ * 
+ * @ORM\Entity
+ * @ORM\Table(name="TaskType")
+ * @SerializerAnnotation\ExclusionPolicy("all")
+ */
+class Type
+{
+    /**
+     * 
+     * @var integer
+     * 
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    
+    /**
+     *
+     * @var string
+     * @ORM\Column(type="string", unique=true, nullable=false)
+     * @SerializerAnnotation\Expose
+     */
+    protected $name;
+    
+    
+    /**
+     *
+     * @var string
+     * @ORM\Column(type="text", nullable=false)
+     */
+    protected $description;
+    
+    
+    /**
+     *
+     * @var SimplyTestable\WorkerBundle\Entity\Task\Type\TaskTypeClass
+     * 
+     * @ORM\ManyToOne(targetEntity="SimplyTestable\WorkerBundle\Entity\Task\Type\TaskTypeClass")
+     * @ORM\JoinColumn(name="tasktypeclass_id", referencedColumnName="id", nullable=false)
+     */
+    protected $class;   
+    
+    /**
+     *
+     * @var boolean
+     * @ORM\Column(type="boolean", name="selectable", nullable=false)
+     */
+    protected $selectable = false;    
+    
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Type
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     * @return Type
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set class
+     *
+     * @param \SimplyTestable\WorkerBundle\Entity\Task\Type\TaskTypeClass $class
+     * @return Type
+     */
+    public function setClass(\SimplyTestable\WorkerBundle\Entity\Task\Type\TaskTypeClass $class)
+    {
+        $this->class = $class;
+        return $this;
+    }
+
+    /**
+     * Get class
+     *
+     * @return \SimplyTestable\WorkerBundle\Entity\Task\Type\TaskTypeClass
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+    
+    
+    public function __construct()
+    {
+        $this->selectable = false;
+    }
+    
+    /**
+     * Set selectable
+     *
+     * @param boolean $selectable
+     * @return Type
+     */
+    public function setSelectable($selectable)
+    {
+        $this->selectable = $selectable;
+        return $this;
+    }
+
+    /**
+     * Get selectable
+     *
+     * @return boolean 
+     */
+    public function getSelectable()
+    {
+        return $this->selectable;
+    }
+    
+    
+    /**
+     *
+     * @param Type $taskType
+     * @return boolean
+     */
+    public function equals(Type $taskType) {
+        return $this->getName() == $taskType->getName();
+    }
+    
+    
+    /**
+     *
+     * @return string
+     */
+    public function __toString() {
+        return $this->getName();
+    }
+}
