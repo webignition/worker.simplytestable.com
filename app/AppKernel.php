ec6c7b3ec5055dcb74df4a8276f2d224a5751113
Jon Cram <jon@webignition.net>
2012-08-10 20:00:06 +0100

    Added resque bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new SimplyTestable\WorkerBundle\SimplyTestableWorkerBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new Glit\ResqueBundle\GlitResqueBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
