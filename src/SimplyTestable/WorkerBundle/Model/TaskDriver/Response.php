81fb730f99abe93a4bf3846bf8170d486da77296
Jon Cram <jon@webignition.net>
2012-09-17 19:28:09 +0100

    Updated webignition/web-page-model

---
+++ src/SimplyTestable/WorkerBundle/Model/TaskDriver/Response.php
@@ -0,0 +1,118 @@
+<?php
+namespace SimplyTestable\WorkerBundle\Model;
+
+/**
+ * 
+ */
+class RemoteEndpoint
+{
+    /**
+     * Unique identifier for this remote endpoint, could be a string, integer, whatever you prefer
+     * 
+     * @var mixed
+     */
+    private $identifier;
+    
+    
+    /**
+     * Full absolute URL to the remote endpoint
+     * 
+     * @var string
+     * 
+     */
+    private $url;
+    
+    
+    /**
+     *
+     * @var int
+     */
+    private $method = HTTP_METH_GET;
+    
+    
+    /**
+     *
+     * @var \HttpRequest
+     */
+    private $httpRequest = null;
+    
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return RemoteEndpoint
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;    
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+    
+    
+    /**
+     * Set identifier
+     *
+     * @param mixed $identifier
+     * @return RemoteEndpoint
+     */
+    public function setIdentifier($identifier)
+    {
+        $this->identifier = $identifier;    
+        return $this;
+    }
+
+    /**
+     * Get identifier
+     *
+     * @return string 
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    } 
+    
+    
+    /**
+     *
+     * @param int $method
+     * @return \SimplyTestable\WorkerBundle\Model\RemoteEndpoint 
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+        return $this;                
+    }
+    
+    /**
+     *
+     * @return int
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+    
+    
+    /**
+     *
+     * @return \HttpRequest
+     */
+    public function getHttpRequest() {
+        if (is_null($this->httpRequest)) {
+            $this->httpRequest = new \HttpRequest($this->getUrl(), $this->getMethod());
+        }
+        
+        return $this->httpRequest;
+    }
+}
---
+++ src/SimplyTestable/WorkerBundle/Services/TaskDriver/W3cValidatorErrorCollectionParser.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace SimplyTestable\WorkerBundle\Services\TaskDriver;
+
+use SimplyTestable\WorkerBundle\Entity\Task\Task;
+use SimplyTestable\WorkerBundle\Entity\Task\Type\Type as TaskType;
+use webignition\WebResource\WebPage\WebPage;
+use webignition\WebResource\JsonDocument\JsonDocument;
+
+class HtmlValidationTaskDriver extends TaskDriver {
+    
+    const DEFAULT_CHARACTER_ENCODING = 'UTF-8';
+    
+    public function execute(Task $task) {
+        if (!$task->getType()->equals($this->getTaskTypeService()->getHtmlValidationTaskType())) {
+            return false;
+        }
+        
+        $resourceRequest = new \HttpRequest($task->getUrl(), HTTP_METH_GET);
+        
+        /* @var $webResource WebPage */
+        $webResource = $this->getWebResourceService()->get($resourceRequest);
+        if (!$webResource instanceof WebPage) {
+            return false;
+        }
+        
+        $characterEncoding = ($webResource->getIsDocumentCharacterEncodingValid()) ? $webResource->getCharacterEncoding() : self::DEFAULT_CHARACTER_ENCODING;                
+        
+        $validationRequest = new \HttpRequest($this->getProperty('validator-url'), HTTP_METH_POST);
+        
+        $requestPostFields = array(
+            'fragment' => $webResource->getContent(),
+            'output' => 'json'
+        );
+        
+        if (!is_null($characterEncoding)) {
+            $requestPostFields['charset'] = $characterEncoding;
+        }
+        
+        $validationRequest->setPostFields($requestPostFields);
+
+        /* @var $validationResponse JsonDocument */
+        $validationResponse = $this->getWebResourceService()->get($validationRequest);
+        
+        $outputObject = $this->getOutputOject($validationResponse->getContentObject());
+        
+        return json_encode($outputObject);
+    }
+    
+    
+    /**
+     *
+     * @return \webignition\InternetMediaType\InternetMediaType 
+     */
+    protected function getOutputContentType()
+    {
+        $mediaTypeParser = new \webignition\InternetMediaType\Parser\Parser();
+        return $mediaTypeParser->parse('application/json');
+    }
+
+
+    
+    /**
+     *
+     * @param \stdClass $validationResponseObject
+     * @return \stdClass 
+     */
+    private function getOutputOject(\stdClass $validationResponseObject) {
+        $outputObject = new \stdClass();
+        $outputObject->messages = array();
+        
+        foreach ($validationResponseObject->messages as $validationMessageObject) {
+            $outputObject->messages[] = $this->getOutputObjectMessage($validationMessageObject);
+        }
+        
+        return $outputObject;
+    }
+    
+    
+    /**
+     *
+     * @param \stdClass $validationMessageObject
+     * @return \stdClass 
+     */
+    private function getOutputObjectMessage(\stdClass $validationMessageObject) {
+        $requiredProperties = array(
+            'lastLine',
+            'lastColumn',
+            'message',
+            'messageid',            
+            'type'
+        );
+        
+        $outputObjectMessage = new \stdClass;
+        
+        foreach ($requiredProperties as $requiredPropertyName) {
+            if (isset($validationMessageObject->$requiredPropertyName)) {
+                $outputObjectMessage->$requiredPropertyName = $validationMessageObject->$requiredPropertyName;
+            }
+        }
+        
+        return $outputObjectMessage;
+    }
+    
+    
+}
