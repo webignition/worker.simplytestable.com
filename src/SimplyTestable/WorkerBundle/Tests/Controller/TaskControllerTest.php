a55217768bb6c27745aeb16dde30787a7e81bdd7
Jon Cram <jon@webignition.net>
2013-01-28 15:12:52 +0000

    Test creation of collection of tasks

--- src/SimplyTestable/WorkerBundle/Tests/Controller/TaskControllerTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Controller/TaskControllerTest.php
@@ -38,7 +38,48 @@ class TaskControllerTest extends BaseControllerJsonTestCase {
         $this->assertEquals($url, $responseObject->url);
         $this->assertEquals('queued', $responseObject->state);
         $this->assertEquals($type, $responseObject->type);          
-    }  
+    }
+    
+    
+    public function testCreateCollectionActionInNormalState() {
+        $thisWorker = $this->getWorkerService()->get();
+        $this->assertTrue($thisWorker->getState()->equals($this->getWorkerService()->getActiveState()));
+        
+        $taskData = array(
+                array(
+                    'url' => 'http://example.com/one/',
+                    'type' => 'HTML validation'
+                ),
+                array(
+                    'url' => 'http://example.com/two/',
+                    'type' => 'CSS validation'
+                ),
+                array(
+                    'url' => 'http://example.com/three/',
+                    'type' => 'JS static analysis'
+                ),              
+        );
+        
+        $_POST = array(
+            'tasks' => $taskData
+        );       
+        
+        /* @var $controller \SimplyTestable\WorkerBundle\Controller\TaskController */
+        $controller = $this->createController(self::TASK_CONTROLLER_NAME, 'createCollectionAction');
+        
+        $response = $controller->createCollectionAction();
+        $responseObject = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(3, count($responseObject));
+        
+        foreach ($responseObject as $responseTaskIndex => $responseTask) {
+            $this->assertEquals($responseTaskIndex + 1, $responseTask->id);
+            $this->assertEquals($taskData[$responseTaskIndex]['url'], $responseTask->url);
+            $this->assertEquals('queued', $responseTask->state);
+            $this->assertEquals($taskData[$responseTaskIndex]['type'], $responseTask->type);               
+        }       
+    }
     
     
     public function testCreateActionInMaintenanceReadOnlyStateReturnsHttp503() {
