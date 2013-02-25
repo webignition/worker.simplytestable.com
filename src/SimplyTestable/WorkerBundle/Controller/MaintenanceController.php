782cbf00d759938e05c76dfb7ec4e9966f6176f1
Jon Cram <jon@webignition.net>
2013-02-25 15:32:40 +0000

    Output all from /leave-read-only

--- src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
+++ src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
@@ -21,9 +21,12 @@ class MaintenanceController extends BaseController
     }
     
     public function leaveReadOnlyAction() {
-        $this->executeCommand('SimplyTestable\WorkerBundle\Command\MaintenanceDisableReadOnlyCommand');
-        $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskReportCompletionEnqueueCommand');  
-        return $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskPerformEnqueueCommand');  
+        $MaintenanceDisableReadOnlyCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\MaintenanceDisableReadOnlyCommand');
+        $TaskReportCompletionEnqueueCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskReportCompletionEnqueueCommand');  
+        $TaskPerformEnqueueCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskPerformEnqueueCommand');
+        
+        $output = $MaintenanceDisableReadOnlyCommandResponse->getContent() . "\n" . $TaskReportCompletionEnqueueCommandResponse->getContent() . "\n" . $TaskPerformEnqueueCommandResponse->getContent();
+        return $this->sendResponse($output);
     }
     
     
