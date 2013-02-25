1eb2b7e04f46a640e6dff758f987f3b7247d9779
Jon Cram <jon@webignition.net>
2013-02-25 15:57:44 +0000

    Output all from /leave-read-only

--- src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
+++ src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
@@ -21,12 +21,25 @@ class MaintenanceController extends BaseController
     }
     
     public function leaveReadOnlyAction() {
-        $MaintenanceDisableReadOnlyCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\MaintenanceDisableReadOnlyCommand');
-        $TaskReportCompletionEnqueueCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskReportCompletionEnqueueCommand');  
-        $TaskPerformEnqueueCommandResponse = $this->executeCommand('SimplyTestable\WorkerBundle\Command\TaskPerformEnqueueCommand');
+        $commands = array(
+            'SimplyTestable\WorkerBundle\Command\MaintenanceDisableReadOnlyCommand',
+            'SimplyTestable\WorkerBundle\Command\TaskReportCompletionEnqueueCommand',
+            'SimplyTestable\WorkerBundle\Command\TaskPerformEnqueueCommand'
+        );
         
-        $output = $MaintenanceDisableReadOnlyCommandResponse->getContent() . "\n" . $TaskReportCompletionEnqueueCommandResponse->getContent() . "\n" . $TaskPerformEnqueueCommandResponse->getContent();
-        return $this->sendResponse($output);
+        $responseLines = array();
+        
+        foreach ($commands as $command) {
+            $response = $this->executeCommand($command);
+            $rawResponseLines =  json_decode($response->getContent());
+            foreach ($rawResponseLines as $rawResponseLine) {
+                if (trim($rawResponseLine) != '') {
+                    $responseLines[] = trim($rawResponseLine);
+                }
+            }
+        }
+        
+        return $this->sendResponse($responseLines);
     }
     
     
