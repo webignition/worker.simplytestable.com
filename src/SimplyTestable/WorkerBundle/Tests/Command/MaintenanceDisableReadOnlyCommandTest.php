470d0ce97da34047523c1f169a4f9ba12a1af791
Jon Cram <jon@webignition.net>
2013-03-11 17:14:29 +0000

    MaintenanceXReadOnlyCommandTest only rebuild db when relevant

--- src/SimplyTestable/WorkerBundle/Tests/Command/MaintenanceDisableReadOnlyCommandTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/MaintenanceDisableReadOnlyCommandTest.php
@@ -5,13 +5,15 @@ namespace SimplyTestable\WorkerBundle\Tests\Command;
 use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
 
 class MaintenanceDisableReadOnlyCommandTest extends ConsoleCommandBaseTestCase {
+    
+    public static function setUpBeforeClass() {
+        self::setupDatabaseIfNotExists();        
+    }       
 
     public function testEnableReadOnlyModeCorrectlyChangesState() {
-        $this->setupDatabase();
         $this->getWorkerService()->setReadOnly();
-      
-        $response = $this->runConsole('simplytestable:maintenance:disable-read-only');        
-        $this->assertEquals(0, $response);
+   
+        $this->assertEquals(0, $this->runConsole('simplytestable:maintenance:disable-read-only'));
         $this->assertEquals('worker-active', $this->getWorkerService()->get()->getState()->getName());
     }
 
--- src/SimplyTestable/WorkerBundle/Tests/Command/MaintenanceEnableReadOnlyCommandTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/MaintenanceEnableReadOnlyCommandTest.php
@@ -5,13 +5,15 @@ namespace SimplyTestable\WorkerBundle\Tests\Command;
 use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
 
 class MaintenanceEnableReadOnlyCommandTest extends ConsoleCommandBaseTestCase {
+    
+    public static function setUpBeforeClass() {
+        self::setupDatabaseIfNotExists();        
+    }     
 
     public function testEnableReadOnlyModeCorrectlyChangesState() {
-        $this->setupDatabase();
-        $this->getWorkerService()->setActive();
-      
-        $response = $this->runConsole('simplytestable:maintenance:enable-read-only');        
-        $this->assertEquals(0, $response);
+        $this->getWorkerService()->setActive();      
+     
+        $this->assertEquals(0, $this->runConsole('simplytestable:maintenance:enable-read-only'));
         $this->assertEquals('worker-maintenance-read-only', $this->getWorkerService()->get()->getState()->getName());
     }
 
