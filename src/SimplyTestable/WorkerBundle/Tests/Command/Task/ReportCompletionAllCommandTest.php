a0bbed338ac636c7211170f1a12b18afbe2a33bf
Jon Cram <jon@webignition.net>
2013-03-17 16:45:25 +0000

    Fix test class name

--- src/SimplyTestable/WorkerBundle/Tests/Command/Task/ReportCompletionAllCommandTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/Task/ReportCompletionAllCommandTest.php
@@ -5,13 +5,13 @@ namespace SimplyTestable\WorkerBundle\Tests\Command\Task;
 use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
 use SimplyTestable\WorkerBundle\Entity\Task\Type\Type as TaskType;
 
-class PerformAllCommandTest extends ConsoleCommandBaseTestCase {
+class ReportCompletionAllCommandTest extends ConsoleCommandBaseTestCase {
     
     public static function setUpBeforeClass() {
         self::setupDatabaseIfNotExists();        
     }    
 
-    public function testPerformAll() {                 
+    public function testReportCompletionAll() {                 
         $task = $this->createTask('http://example.com/', 'HTML validation');        
         $response = $this->runConsole('simplytestable:task:reportcompletion:all');
         
