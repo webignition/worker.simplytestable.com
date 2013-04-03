2f7dc8bd852d63c36aeff4d00fdc7d1eeb1019ea
Jon Cram <jon@webignition.net>
2013-04-03 08:59:48 +0100

    Added live html validation test

---
+++ src/SimplyTestable/WorkerBundle/Tests/Live/Command/Task/Perform/HtmlValidation/PerformHtmlValidationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SimplyTestable\WorkerBundle\Tests\Live\Command\Task\Perform\HtmlValidation;
+
+use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
+
+class PerformHtmlValidationTest extends ConsoleCommandBaseTestCase {
+    
+    public static function setUpBeforeClass() {
+        self::setupDatabase();        
+    }    
+
+    /**
+     * @group live
+     */    
+    public function testErrorFreeHtmlValidation() {        
+        $taskObject = $this->createTask('http://html-validation.simplytestable.com', 'HTML validation');
+        
+        $task = $this->getTaskService()->getById($taskObject->id);
+        
+        $response = $this->runConsole('simplytestable:task:perform', array(
+            $task->getId() => true
+        ));
+
+        $this->assertEquals(0, $response);
+        $this->assertEquals(0, $task->getOutput()->getErrorCount());        
+        $this->assertEquals('{"messages":[]}', $task->getOutput()->getOutput());
+    }    
+
+
+}
