572acdc8ef40897e89e3ee264fa0b5de5fc84c2b
Jon Cram <jon@webignition.net>
2013-04-03 15:17:10 +0100

    Test error-free html5 minimal with and without utf-8 bom

---
+++ src/SimplyTestable/WorkerBundle/Tests/Live/Command/Task/Perform/HtmlValidation/HTML5/PerformHtmlValidationTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SimplyTestable\WorkerBundle\Tests\Live\Command\Task\Perform\HtmlValidation\HTML5;
+
+use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
+
+class PerformHtmlValidationTest extends ConsoleCommandBaseTestCase {
+    
+    public static function setUpBeforeClass() {
+        self::setupDatabase();        
+    }    
+
+    /**
+     * @group live
+     * @group live-travis
+     */    
+    public function testErrorFreeHtmlValidation() {        
+        $taskObject = $this->createTask('http://html-validation.simplytestable.com', 'HTML validation');
+        
+        $task = $this->getTaskService()->getById($taskObject->id);
+        
+        $response = $this->runConsole('simplytestable:task:perform', array(
+            $task->getId() => true
+        ));
+
+        $this->assertEquals(0, $response);
+        $this->assertEquals(0, $task->getOutput()->getErrorCount());        
+        $this->assertEquals('{"messages":[]}', $task->getOutput()->getOutput());
+    }  
+    
+    
+    /**
+     * @group live
+     * @group live-travis
+     */    
+    public function testMinimalNoErrors() {        
+        $taskObject = $this->createTask('http://html-validation.simplytestable.com/html5/minimal-no-errors.html', 'HTML validation');
+        
+        $task = $this->getTaskService()->getById($taskObject->id);
+        
+        $response = $this->runConsole('simplytestable:task:perform', array(
+            $task->getId() => true
+        ));
+
+        $this->assertEquals(0, $response);
+        $this->assertEquals(0, $task->getOutput()->getErrorCount());        
+        $this->assertEquals('{"messages":[]}', $task->getOutput()->getOutput());
+    } 
+    
+    
+    /**
+     * @group live
+     * @group live-travis
+     */    
+    public function testMinimalNoErrorsWithUtf8Bom() {        
+        $taskObject = $this->createTask('http://html-validation.simplytestable.com/html5/minimal-with-utf8-bom.html', 'HTML validation');
+        
+        $task = $this->getTaskService()->getById($taskObject->id);
+        
+        $response = $this->runConsole('simplytestable:task:perform', array(
+            $task->getId() => true
+        ));
+
+        $this->assertEquals(0, $response);
+        $this->assertEquals(0, $task->getOutput()->getErrorCount());        
+        $this->assertEquals('{"messages":[]}', $task->getOutput()->getOutput());
+    }      
+
+
+}
