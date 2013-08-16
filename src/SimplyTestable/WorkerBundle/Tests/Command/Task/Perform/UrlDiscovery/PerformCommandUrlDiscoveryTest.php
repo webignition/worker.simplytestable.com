d79de169f670029beb83c409360ff713a3814706
Jon Cram <jon@webignition.net>
2013-08-16 21:33:33 +0100

    Allow url discovery scope to take a collection of values

--- src/SimplyTestable/WorkerBundle/Tests/Command/Task/Perform/UrlDiscovery/PerformCommandUrlDiscoveryTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/Task/Perform/UrlDiscovery/PerformCommandUrlDiscoveryTest.php
@@ -72,7 +72,25 @@ class PerformCommandUrlDiscoveryTest extends ConsoleCommandBaseTestCase {
             "http://example.com/articles/i-make-the-internet/",
             "http://example.com/articles/getting-to-building-simpytestable-dot-com/"
         ), json_decode($task->getOutput()->getOutput()));
-    }    
+    } 
+    
+    
+    
+    /**
+     * @group standard
+     */    
+    public function testTreatWwwAndNonWwwAsEquivalent() {
+        $this->clearMemcacheHttpCache();  
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));
 
+        $taskObject = $this->createTask('http://example.com/', 'URL discovery', '{"scope":["http:\/\/example.com","http:\/\/www.example.com"]}');
+        
+        $task = $this->getTaskService()->getById($taskObject->id);
 
+        $this->assertEquals(0, $this->runConsole('simplytestable:task:perform', array(
+            $task->getId() => true
+        )));
+        
+        $this->assertEquals(31, count(json_decode($task->getOutput()->getOutput())));
+    } 
 }
