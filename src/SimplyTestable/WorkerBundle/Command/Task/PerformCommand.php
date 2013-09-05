7bfea6305df23516d83ec6ec6a660c3a0ca5621c
Jon Cram <jon@webignition.net>
2013-09-05 18:29:33 +0100

    Don't dump full exception when task perform fails

--- src/SimplyTestable/WorkerBundle/Command/Task/PerformCommand.php
+++ src/SimplyTestable/WorkerBundle/Command/Task/PerformCommand.php
@@ -55,8 +55,8 @@ EOF
         } catch (\Exception $e) {
             var_dump('Exception calling $this->getTaskService()->perform');
             var_dump('Exception class: '.get_class($e));
-            var_dump($e);
-            exit();
+//            var_dump($e);
+//            exit();
         }
         
         if ($performResult === 0) {
