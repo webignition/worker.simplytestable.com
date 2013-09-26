80fd9a898f7d99790a86ca2b77aa0b0ea6c5eab9
Jon Cram <jon@webignition.net>
2013-09-26 22:11:35 +0100

    Load fixtures after rebuilding local db

--- tools/resetdb-dev.sh
+++ tools/resetdb-dev.sh
@@ -1,3 +1,4 @@
 php app/console doctrine:database:drop -e dev --force
 php app/console doctrine:database:create -e dev
 php app/console doctrine:migrations:migrate -e dev --no-interaction
+php app/console doctrine:fixtures:load -e dev --append
