59bf7a37035db931e08b47ee7af78d0e7c1ee9f9
Jon Cram <jon@webignition.net>
2013-09-26 20:32:23 +0100

    Add fixtures bundle to kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Glit\ResqueBundle\GlitResqueBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
