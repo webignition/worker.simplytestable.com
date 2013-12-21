8c10a03f572bbfe841ad2f25853cc3c309c80fce
Jon Cram <jon@webignition.net>
2013-12-21 18:39:20 +0000

    Remove travis guzzle patch

--- travis-ci/patch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/travis/webignition/worker.simplytestable.com
-sed -i '80s/.*/        $exception = $event['\''exception'\''] instanceof \Guzzle\Http\Exception\CurlException ? $event['\''exception'\''] : null;/' vendor/guzzle/guzzle/src/Guzzle/Plugin/Backoff/BackoffPlugin.php
-cat vendor/guzzle/guzzle/src/Guzzle/Plugin/Backoff/BackoffPlugin.php
