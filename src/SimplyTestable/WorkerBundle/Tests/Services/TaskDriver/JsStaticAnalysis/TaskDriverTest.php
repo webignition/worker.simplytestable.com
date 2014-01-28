f3bc5459bf5557f599c89b7a2963f22573ce7dc1
Jon Cram <jon@webignition.net>
2014-01-28 15:50:41 +0000

    Remove defering to parent node-jslint wrapper for output

--- src/SimplyTestable/WorkerBundle/Tests/Services/TaskDriver/JsStaticAnalysis/TaskDriverTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Services/TaskDriver/JsStaticAnalysis/TaskDriverTest.php
@@ -5,12 +5,6 @@ namespace SimplyTestable\WorkerBundle\Tests\Services\TaskDriver\JsStaticAnalysis
 use SimplyTestable\WorkerBundle\Tests\Services\TaskDriver\BaseTest;
 
 abstract class TaskDriverTest extends BaseTest {
-    
-    public function setUp() {
-        parent::setUp();
-
-        $this->container->get('simplytestable.services.nodeJslintWrapperService')->enableDeferToParentIfNoRawOutput();
-    }
 
     protected function getTaskTypeName() {
         return 'JS Static Analysis';
