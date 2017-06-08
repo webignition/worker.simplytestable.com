f9d5ff619d2f1c5d724cb5013c2abe2cf47f48df
Jon Cram <jon@webignition.net>
2017-06-08 14:49:24 +0100

    Remove unnecessary tests

--- src/SimplyTestable/WorkerBundle/Tests/Services/TaskDriver/CssValidation/HandleMalformedContentType/NoSemiColonBetweenTypeAndParameterTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace SimplyTestable\WorkerBundle\Tests\Services\TaskDriver\CssValidation\HandleMalformedContentType;
-
-class NoSemiColonBetweenTypeAndParameterTest extends HandleMalformedContentTypeTest {
-}
