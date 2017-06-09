6dff7589f14e6344f7162358dcbef19e96c174c3
Jon Cram <jon@webignition.net>
2017-06-09 11:23:06 +0100

    partial fix tests

--- src/SimplyTestable/WorkerBundle/Tests/Command/Task/PerformEnqueueCommandTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/Task/PerformEnqueueCommandTest.php
@@ -2,32 +2,33 @@
 
 namespace SimplyTestable\WorkerBundle\Tests\Command\Task;
 
+use SimplyTestable\WorkerBundle\Command\Task\PerformEnqueueCommand;
 use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
+use SimplyTestable\WorkerBundle\Tests\Factory\TaskFactory;
 
-class PerformEnqueueCommandTest extends ConsoleCommandBaseTestCase {
-    
-    protected function getAdditionalCommands() {
+class PerformEnqueueCommandTest extends ConsoleCommandBaseTestCase
+{
+    protected function getAdditionalCommands()
+    {
         return array(
-            new \SimplyTestable\WorkerBundle\Command\Task\PerformEnqueueCommand()
+            new PerformEnqueueCommand()
         );
     }
 
-
-    public function setUp() {
+    protected function setUp()
+    {
         parent::setUp();
         $this->clearRedis();
     }
 
-    public function tearDown() {
+    protected function tearDown()
+    {
         $this->clearRedis();
         parent::tearDown();
     }
-    
-    
-    /**
-     * @group standard
-     */    
-    public function testEnqueueTaskPerformJobs() {        
+
+    public function testEnqueueTaskPerformJobs()
+    {
         $taskPropertyCollection = array(
             array(
                 'url' => 'http://example.com/1/',
@@ -44,26 +45,28 @@ class PerformEnqueueCommandTest extends ConsoleCommandBaseTestCase {
             array(
                 'url' => 'http://example.com/2/',
                 'type' => 'HTML validation'
-            ),            
+            ),
             array(
                 'url' => 'http://example.com/3/',
                 'type' => 'HTML validation'
-            ),             
+            ),
         );
-        
-        $tasks = array();        
+
+        $tasks = array();
         foreach ($taskPropertyCollection as $taskProperties) {
-            $tasks[] = $this->createTask($taskProperties['url'], $taskProperties['type']);
+            $tasks[] = $this->getTaskFactory()->create(TaskFactory::createTaskValuesFromDefaults(
+                $taskProperties
+            ));
         }
 
         $this->clearRedis();
-  
+
         $this->assertEquals(0, $this->executeCommand('simplytestable:task:perform:enqueue'));
-        
+
         foreach ($tasks as $task) {
             $this->assertTrue($this->getRequeQueueService()->contains('task-perform', array(
-                'id' => $task->id
-            )));             
+                'id' => $task->getId()
+            )));
         }
     }
 }
--- src/SimplyTestable/WorkerBundle/Tests/Command/Task/ReportCompletionAllCommandTest.php
+++ src/SimplyTestable/WorkerBundle/Tests/Command/Task/ReportCompletionAllCommandTest.php
@@ -2,38 +2,38 @@
 
 namespace SimplyTestable\WorkerBundle\Tests\Command\Task;
 
+use SimplyTestable\WorkerBundle\Command\Task\ReportCompletionAllCommand;
 use SimplyTestable\WorkerBundle\Tests\Command\ConsoleCommandBaseTestCase;
 use SimplyTestable\WorkerBundle\Entity\TimePeriod;
+use SimplyTestable\WorkerBundle\Tests\Factory\TaskFactory;
 
-class ReportCompletionAllCommandTest extends ConsoleCommandBaseTestCase {
-    
-    protected function getAdditionalCommands() {
+class ReportCompletionAllCommandTest extends ConsoleCommandBaseTestCase
+{
+    protected function getAdditionalCommands()
+    {
         return array(
-            new \SimplyTestable\WorkerBundle\Command\Task\ReportCompletionAllCommand()
+            new ReportCompletionAllCommand()
         );
-    }   
+    }
 
-    
-    /**
-     * @group standard
-     */    
-    public function testReportCompletionAll() {        
-        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));        
+    public function testReportCompletionAll()
+    {
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__ . '/HttpResponses')));
         $this->removeAllTasks();
-        
-        $taskProperties = $this->createTask('http://example.com/', 'HTML validation');        
-        
-        $task = $this->getTaskService()->getById($taskProperties->id);
+
+        $task = $this->getTaskFactory()->create(TaskFactory::createTaskValuesFromDefaults([
+            'url' => 'http://example.com/',
+            'type' => 'html validation',
+        ]));
+
         $taskTimePeriod = new TimePeriod();
         $taskTimePeriod->setStartDateTime(new \DateTime('1970-01-01'));
-        $taskTimePeriod->setEndDateTime(new \DateTime('1970-01-02'));        
-        
+        $taskTimePeriod->setEndDateTime(new \DateTime('1970-01-02'));
+
         $task->setTimePeriod($taskTimePeriod);
-        
+
         $this->createCompletedTaskOutputForTask($task);
-        
+
         $this->assertEquals(0, $this->executeCommand('simplytestable:task:reportcompletion:all'));
-    }  
-
-
+    }
 }
