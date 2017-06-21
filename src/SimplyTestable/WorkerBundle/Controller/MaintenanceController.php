a4c5cdfea08bace9bd722f1067deb671601e40f6
Jon Cram <jon@webignition.net>
2017-06-21 18:56:53 +0100

    Fix maintenance controller command calling

--- src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
+++ src/SimplyTestable/WorkerBundle/Controller/MaintenanceController.php
@@ -8,31 +8,39 @@ use SimplyTestable\WorkerBundle\Command\Task\PerformEnqueueCommand;
 use SimplyTestable\WorkerBundle\Command\Task\ReportCompletionEnqueueCommand;
 use SimplyTestable\WorkerBundle\Output\StringOutput;
 use SimplyTestable\WorkerBundle\Services\CommandService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\HttpFoundation\Response;
 
 class MaintenanceController extends BaseController
 {
     public function enableReadOnlyAction()
     {
-        return $this->executeCommand(EnableReadOnlyCommand::class);
+        return $this->executeCommand(new EnableReadOnlyCommand(
+            $this->container->get('simplytestable.services.workerservice')
+        ));
     }
 
     public function disableReadOnlyAction()
     {
-        return $this->executeCommand(DisableReadOnlyCommand::class);
+        return $this->executeCommand($this->createDisableReadOnlyCommand());
     }
 
     public function taskPerformEnqueueAction()
     {
-        return $this->executeCommand(PerformEnqueueCommand::class);
+        return $this->executeCommand(new PerformEnqueueCommand(
+            $this->container->get('simplytestable.services.taskservice'),
+            $this->container->get('simplytestable.services.resque.queueservice'),
+            $this->container->get('simplytestable.services.resque.jobfactoryservice')
+        ));
     }
 
     public function leaveReadOnlyAction()
     {
         $commands = [
-            DisableReadOnlyCommand::class,
-            ReportCompletionEnqueueCommand::class,
-            PerformEnqueueCommand::class
+            $this->createDisableReadOnlyCommand(),
+            $this->createReportCompletionEnqueueCommand(),
+            $this->createPerformEnqueueCommand()
         ];
 
         $responseLines = array();
@@ -51,30 +59,52 @@ class MaintenanceController extends BaseController
     }
 
     /**
-     * @param string $commandClass
-     * @param array $inputArray
+     * @return DisableReadOnlyCommand
+     */
+    private function createDisableReadOnlyCommand()
+    {
+        return new DisableReadOnlyCommand(
+            $this->container->get('simplytestable.services.workerservice')
+        );
+    }
+
+    /**
+     * @return ReportCompletionEnqueueCommand
+     */
+    private function createReportCompletionEnqueueCommand()
+    {
+        return new ReportCompletionEnqueueCommand(
+            $this->container->get('logger'),
+            $this->container->get('simplytestable.services.taskservice'),
+            $this->container->get('simplytestable.services.resque.queueservice'),
+            $this->container->get('simplytestable.services.resque.jobfactoryservice')
+        );
+    }
+
+    /**
+     * @return PerformEnqueueCommand
+     */
+    private function createPerformEnqueueCommand()
+    {
+        return new PerformEnqueueCommand(
+            $this->container->get('simplytestable.services.taskservice'),
+            $this->container->get('simplytestable.services.resque.queueservice'),
+            $this->container->get('simplytestable.services.resque.jobfactoryservice')
+        );
+    }
+
+    /**
+     * @param Command $command
      *
      * @return Response
      */
-    private function executeCommand($commandClass, $inputArray = array())
+    private function executeCommand(Command $command)
     {
         $output = new StringOutput();
-        $commandResponse =  $this->getCommandService()->execute(
-                $commandClass,
-                $inputArray,
-                $output
-        );
+        $commandResponse = $command->run(new ArrayInput([]), $output);
 
         $outputLines = explode("\n", trim($output->getBuffer()));
 
         return $this->sendResponse($outputLines, $commandResponse === 0 ? 200 : 500);
-    }
-
-    /**
-     * @return CommandService
-     */
-    private function getCommandService()
-    {
-        return $this->container->get('simplytestable.services.commandService');
     }
 }
