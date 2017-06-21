19f01dde69a98e657b179aa31871cf3af517952d
Jon Cram <jon@webignition.net>
2017-06-21 18:22:47 +0100

    Servicify Task/ReportCompletionEnqueueCommand

--- src/SimplyTestable/WorkerBundle/Command/Task/ReportCompletionEnqueueCommand.php
+++ src/SimplyTestable/WorkerBundle/Command/Task/ReportCompletionEnqueueCommand.php
@@ -1,11 +1,58 @@
 <?php
 namespace SimplyTestable\WorkerBundle\Command\Task;
 
+use Psr\Log\LoggerInterface;
+use SimplyTestable\WorkerBundle\Services\Resque\JobFactoryService as ResqueJobFactoryService;
+use SimplyTestable\WorkerBundle\Services\Resque\QueueService as ResqueQueueService;
+use SimplyTestable\WorkerBundle\Services\TaskService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as BaseCommand;
 
-class ReportCompletionEnqueueCommand extends Command
+class ReportCompletionEnqueueCommand extends BaseCommand
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var TaskService
+     */
+    private $taskService;
+
+    /**
+     * @var ResqueQueueService
+     */
+    private $resqueQueueService;
+
+    /**
+     * @var ResqueJobFactoryService
+     */
+    private $resqueJobFactoryService;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param TaskService $taskService
+     * @param ResqueQueueService $resqueQueueService
+     * @param ResqueJobFactoryService $resqueJobFactoryService
+     * @param string|null $name
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        TaskService $taskService,
+        ResqueQueueService $resqueQueueService,
+        ResqueJobFactoryService $resqueJobFactoryService,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->logger = $logger;
+        $this->taskService = $taskService;
+        $this->resqueQueueService = $resqueQueueService;
+        $this->resqueJobFactoryService = $resqueJobFactoryService;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -22,24 +69,24 @@ class ReportCompletionEnqueueCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $taskIds = $this->getTaskService()->getEntityRepository()->getIdsWithOutput();
+        $taskIds = $this->taskService->getEntityRepository()->getIdsWithOutput();
 
-        $logger = $this->getContainer()->get('logger');
-
-        $logger->info('TaskReportCompletionEnqueueCommand::initialise');
-        $logger->info('TaskReportCompletionEnqueueCommand::'.count($taskIds).' completed tasks ready to be enqueued');
+        $this->logger->info('TaskReportCompletionEnqueueCommand::initialise');
+        $this->logger->info(
+            'TaskReportCompletionEnqueueCommand::'.count($taskIds).' completed tasks ready to be enqueued'
+        );
         $output->writeln(count($taskIds).' completed tasks ready to be enqueued');
 
         foreach ($taskIds as $taskId) {
-            if ($this->getResqueQueueService()->contains('task-report-completion', array('id' => $taskId))) {
+            if ($this->resqueQueueService->contains('task-report-completion', array('id' => $taskId))) {
                 $output->writeln('Task ['.$taskId.'] is already enqueued');
-                $logger->info('TaskReportCompletionEnqueueCommand::Task ['.$taskId.'] is already enqueued');
+                $this->logger->info('TaskReportCompletionEnqueueCommand::Task ['.$taskId.'] is already enqueued');
             } else {
                 $output->writeln('Enqueuing task ['.$taskId.']');
-                $logger->info('TaskReportCompletionEnqueueCommand::Enqueuing task ['.$taskId.']');
+                $this->logger->info('TaskReportCompletionEnqueueCommand::Enqueuing task ['.$taskId.']');
 
-                $this->getResqueQueueService()->enqueue(
-                    $this->getResqueJobFactoryService()->create(
+                $this->resqueQueueService->enqueue(
+                    $this->resqueJobFactoryService->create(
                         'task-report-completion',
                         ['id' => $taskId]
                     )
