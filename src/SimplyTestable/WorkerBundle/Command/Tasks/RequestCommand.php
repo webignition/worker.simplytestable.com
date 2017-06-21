13b6019c915c390636780c62b9837e00c87f6682
Jon Cram <jon@webignition.net>
2017-06-21 18:29:12 +0100

    Servicify Tasks/RequestCommand

--- src/SimplyTestable/WorkerBundle/Command/Tasks/RequestCommand.php
+++ src/SimplyTestable/WorkerBundle/Command/Tasks/RequestCommand.php
@@ -1,17 +1,64 @@
 <?php
 namespace SimplyTestable\WorkerBundle\Command\Tasks;
 
+use SimplyTestable\WorkerBundle\Services\Resque\JobFactoryService as ResqueJobFactoryService;
+use SimplyTestable\WorkerBundle\Services\Resque\QueueService as ResqueQueueService;
+use SimplyTestable\WorkerBundle\Services\TasksService;
+use SimplyTestable\WorkerBundle\Services\WorkerService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use SimplyTestable\WorkerBundle\Exception\Services\TasksService\RequestException;
+use Symfony\Component\Console\Command\Command as BaseCommand;
 
-class RequestCommand extends Command
+class RequestCommand extends BaseCommand
 {
     const RETURN_CODE_OK = 0;
     const RETURN_CODE_FAILED = 1;
     const RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE = 2;
     const RETURN_CODE_TASK_WORKLOAD_EXCEEDS_REQUEST_THRESHOLD = 3;
+
+    /**
+     * @var TasksService
+     */
+    private $tasksService;
+
+    /**
+     * @var WorkerService
+     */
+    private $workerService;
+
+    /**
+     * @var ResqueQueueService
+     */
+    private $resqueQueueService;
+
+    /**
+     * @var ResqueJobFactoryService
+     */
+    private $resqueJobFactoryService;
+
+    /**
+     * @param TasksService $tasksService
+     * @param WorkerService $workerService
+     * @param ResqueQueueService $resqueQueueService
+     * @param ResqueJobFactoryService $resqueJobFactoryService
+     * @param string|null $name
+     */
+    public function __construct(
+        TasksService $tasksService,
+        WorkerService $workerService,
+        ResqueQueueService $resqueQueueService,
+        ResqueJobFactoryService $resqueJobFactoryService,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->tasksService = $tasksService;
+        $this->workerService = $workerService;
+        $this->resqueQueueService = $resqueQueueService;
+        $this->resqueJobFactoryService = $resqueJobFactoryService;
+    }
 
     /**
      * {@inheritdoc}
@@ -30,14 +77,14 @@ class RequestCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($this->getWorkerService()->isMaintenanceReadOnly()) {
+        if ($this->workerService->isMaintenanceReadOnly()) {
             $this->requeue();
 
             return self::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE;
         }
 
         try {
-            if ($this->getTasksService()->request($input->getArgument('limit'))) {
+            if ($this->tasksService->request($input->getArgument('limit'))) {
                 return self::RETURN_CODE_OK;
             }
 
@@ -52,9 +99,9 @@ class RequestCommand extends Command
 
     private function requeue()
     {
-        if ($this->getResqueQueueService()->isEmpty('tasks-request')) {
-            $this->getResqueQueueService()->enqueue(
-                $this->getResqueJobFactoryService()->create(
+        if ($this->resqueQueueService->isEmpty('tasks-request')) {
+            $this->resqueQueueService->enqueue(
+                $this->resqueJobFactoryService->create(
                     'tasks-request'
                 )
             );
