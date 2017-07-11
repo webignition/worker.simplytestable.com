6a583771ac43c2499bf502d723c643edd58eff44
Jon Cram <jon@webignition.net>
2017-07-11 17:07:04 +0100

    Move curl_options param

--- src/SimplyTestable/WorkerBundle/DependencyInjection/SimplyTestableWorkerExtension.php
+++ src/SimplyTestable/WorkerBundle/DependencyInjection/SimplyTestableWorkerExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +23,14 @@ class SimplyTestableWorkerExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $fileLocator = new FileLocator([
+            __DIR__.'/../Resources/config',
+        ]);
+
+        $loader = new Loader\YamlFileLoader($container, $fileLocator);
         $loader->load('services.yml');
+
+        $curlOptionsData = Yaml::parse(file_get_contents($fileLocator->locate('curl_options.yml')));
+        $container->setParameter('curl_options', $curlOptionsData);
     }
 }
