c9ba06236085643bbac3412b531efc8d1a549a57
Jon Cram <jon@webignition.net>
2017-12-01 11:54:46 +0000

    Re-implement Resque job functional tests as unit tests

---
+++ tests/WorkerBundle/Unit/Resque/Job/TasksRequestJobTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\WorkerBundle\Unit\Resque\Job;
+
+use Mockery\Mock;
+use Psr\Log\LoggerInterface;
+use SimplyTestable\WorkerBundle\Command\Tasks\RequestCommand;
+use SimplyTestable\WorkerBundle\Resque\Job\TasksRequestJob;
+
+class TasksRequestJobTest extends AbstractJobTest
+{
+    /**
+     * @throws \Exception
+     */
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        /* @var Mock|LoggerInterface $logger */
+        $logger = \Mockery::mock(LoggerInterface::class);
+
+        $logger
+            ->shouldReceive('error')
+            ->with(sprintf(
+                '%s: task [default] returned %d',
+                TasksRequestJob::class,
+                RequestCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE
+            ));
+
+        $logger
+            ->shouldReceive('error')
+            ->with(sprintf(
+                '%s: task [default] output ',
+                TasksRequestJob::class
+            ));
+
+        $jobArgs = [];
+
+        $job = new TasksRequestJob();
+
+        $this->createJob(
+            $job,
+            $jobArgs,
+            RequestCommand::class,
+            RequestCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $logger
+        );
+
+        $job->run($jobArgs);
+    }
+}
