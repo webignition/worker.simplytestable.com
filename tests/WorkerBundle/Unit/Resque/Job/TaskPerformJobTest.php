283e226905ff66778b2b9d0b6be19e12796b73cf
Jon Cram <webignition@gmail.com>
2017-12-01 11:57:28 +0000

    Re-implement Resque job functional tests as unit tests (#70)

---
+++ tests/WorkerBundle/Unit/Resque/Job/TaskPerformJobTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\WorkerBundle\Unit\Resque\Job;
+
+use Mockery\Mock;
+use Psr\Log\LoggerInterface;
+use SimplyTestable\WorkerBundle\Command\Task\PerformCommand;
+use SimplyTestable\WorkerBundle\Resque\Job\TaskPerformJob;
+
+class TaskPerformJobTest extends AbstractJobTest
+{
+    /**
+     * @throws \Exception
+     */
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        $taskId = 1;
+
+        /* @var Mock|LoggerInterface $logger */
+        $logger = \Mockery::mock(LoggerInterface::class);
+        $logger
+            ->shouldReceive('error')
+            ->with(sprintf(
+                '%s: task [%d] returned %d',
+                TaskPerformJob::class,
+                $taskId,
+                PerformCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE
+            ));
+
+        $logger
+            ->shouldReceive('error')
+            ->with(sprintf(
+                '%s: task [%d] output ',
+                TaskPerformJob::class,
+                $taskId
+            ));
+
+        $jobArgs = [
+            'id' => $taskId,
+        ];
+
+        $job = new TaskPerformJob();
+
+        $this->createJob(
+            $job,
+            $jobArgs,
+            PerformCommand::class,
+            PerformCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $logger
+        );
+
+        $job->run($jobArgs);
+    }
+}
