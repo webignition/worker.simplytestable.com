86f06e8f366fc6d69ed53b517392b88fe52e0fac
Jon Cram <jon@webignition.net>
2018-04-10 12:12:06 +0100

    Simplify HttpAuthenticationMiddlewareTest

--- tests/WorkerBundle/Functional/Services/GuzzleMiddleware/HttpAuthenticationMiddlewareTest.php
+++ tests/WorkerBundle/Functional/Services/GuzzleMiddleware/HttpAuthenticationMiddlewareTest.php
@@ -2,17 +2,12 @@
 
 namespace Tests\WorkerBundle\Functional\Services\GuzzleMiddleware;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
-use SimplyTestable\WorkerBundle\Services\GuzzleMiddleware\HttpAuthenticationMiddleware;
+use SimplyTestable\WorkerBundle\Services\FooHttpClientService;
 use Tests\WorkerBundle\Functional\AbstractBaseTestCase;
-use webignition\HttpHistoryContainer\Container as HttpHistoryContainer;
 
 class HttpAuthenticationMiddlewareTest extends AbstractBaseTestCase
 {
@@ -34,25 +29,20 @@ class HttpAuthenticationMiddlewareTest extends AbstractBaseTestCase
         $domain,
         $expectedAuthorizationHeader
     ) {
-        $httpHistoryContainer = $this->container->get(HttpHistoryContainer::class);
+        $fooHttpClientService = $this->container->get(FooHttpClientService::class);
 
-        $mockHandler = new MockHandler([
+        $fooHttpClientService->appendFixtures([
             new Response(),
         ]);
 
-        $httpAuthenticationMiddleware = new HttpAuthenticationMiddleware();
-        $httpAuthenticationMiddleware->setUsername($username);
-        $httpAuthenticationMiddleware->setPassword($password);
-        $httpAuthenticationMiddleware->setDomain($domain);
+        $httpClient = $fooHttpClientService->getHttpClient();
+        $httpHistory = $fooHttpClientService->getHistory();
 
-        $handlerStack = HandlerStack::create($mockHandler);
-        $handlerStack->push($httpAuthenticationMiddleware, 'http_auth');
-        $handlerStack->push(Middleware::history($httpHistoryContainer), 'history');
+        $fooHttpClientService->setBasicHttpAuthorization($username, $password, $domain);
 
-        $client = new Client(['handler' => $handlerStack]);
-        $client->send($request);
+        $httpClient->send($request);
 
-        $lastRequest = $httpHistoryContainer->getLastRequest();
+        $lastRequest = $httpHistory->getLastRequest();
         $this->assertEquals($expectedAuthorizationHeader, $lastRequest->getHeaderLine('authorization'));
     }
 
