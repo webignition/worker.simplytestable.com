7013f92503be453dfa8bb27a125d8399e7d97be3
Jon Cram <webignition@gmail.com>
2018-07-13 10:23:11 +0100

    Move Resources/config/link_integrity_user_agents.yml (#119)

--- src/SimplyTestable/WorkerBundle/DependencyInjection/SimplyTestableWorkerExtension.php
+++ src/SimplyTestable/WorkerBundle/DependencyInjection/SimplyTestableWorkerExtension.php
@@ -3,10 +3,7 @@
 namespace SimplyTestable\WorkerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -15,10 +12,6 @@ use Symfony\Component\Yaml\Yaml;
  */
 class SimplyTestableWorkerExtension extends Extension
 {
-    private $parameterFiles = [
-        'link_integrity_user_agents.yml',
-    ];
-
     /**
      * {@inheritdoc}
      */
@@ -26,17 +19,5 @@ class SimplyTestableWorkerExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $fileLocator = new FileLocator([
-            __DIR__.'/../Resources/config',
-        ]);
-
-        foreach ($this->parameterFiles as $parameterFile) {
-            $parameterName = str_replace('.yml', '', $parameterFile);
-            $container->setParameter(
-                $parameterName,
-                Yaml::parse(file_get_contents($fileLocator->locate($parameterFile)))
-            );
-        }
     }
 }
