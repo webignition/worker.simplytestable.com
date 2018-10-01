18f28b43d6cabe0474a7137c4cec56df6e839b19
Jon Cram <jon@webignition.net>
2018-10-01 16:37:34 +0100

    Inject task commmands into task collection commands

--- src/Command/Tasks/PerformCommand.php
+++ src/Command/Tasks/PerformCommand.php
@@ -2,65 +2,29 @@
 
 namespace App\Command\Tasks;
 
-use App\Services\TaskPerformer;
-use Psr\Log\LoggerInterface;
 use App\Command\Task\PerformCommand as TaskPerformCommand;
-use App\Services\Resque\QueueService as ResqueQueueService;
 use App\Services\TaskService;
-use App\Services\WorkerService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PerformCommand extends AbstractTaskCollectionCommand
 {
     /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
      * @var TaskService
      */
     private $taskService;
 
     /**
-     * @var WorkerService
+     * @var TaskPerformCommand
      */
-    private $workerService;
+    private $taskPerformCommand;
 
-    /**
-     * @var ResqueQueueService
-     */
-    private $resqueQueueService;
+    public function __construct(TaskService $taskService, TaskPerformCommand $taskPerformCommand)
+    {
+        parent::__construct(null);
 
-    /**
-     * @var TaskPerformer
-     */
-    private $taskPerformanceService;
-
-    /**
-     * @param LoggerInterface $logger
-     * @param TaskService $taskService
-     * @param WorkerService $workerService
-     * @param ResqueQueueService $resqueQueueService
-     * @param TaskPerformer $taskPerformanceService
-     * @param string|null $name
-     */
-    public function __construct(
-        LoggerInterface $logger,
-        TaskService $taskService,
-        WorkerService $workerService,
-        ResqueQueueService $resqueQueueService,
-        TaskPerformer $taskPerformanceService,
-        $name = null
-    ) {
-        parent::__construct($name);
-
-        $this->logger = $logger;
         $this->taskService = $taskService;
-        $this->workerService = $workerService;
-        $this->resqueQueueService = $resqueQueueService;
-        $this->taskPerformanceService = $taskPerformanceService;
+        $this->taskPerformCommand = $taskPerformCommand;
     }
 
     /**
@@ -80,18 +44,9 @@ class PerformCommand extends AbstractTaskCollectionCommand
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $taskIds = $this->taskService->getQueuedTaskIds();
-
         $output->writeln(count($taskIds).' queued tasks ready to be performed');
 
-        $performCommand = new TaskPerformCommand(
-            $this->logger,
-            $this->taskService,
-            $this->workerService,
-            $this->resqueQueueService,
-            $this->taskPerformanceService
-        );
-
-        $this->executeForCollection($taskIds, $performCommand, $output);
+        $this->executeForCollection($taskIds, $this->taskPerformCommand, $output);
 
         return 0;
     }
