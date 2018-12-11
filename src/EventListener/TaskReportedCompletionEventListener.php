cde67fc366ab1b6ecca8b6aabe469b19bc37e0f4
Jon Cram <jon@webignition.net>
2018-12-11 15:23:25 +0000

    Create TaskReportedCompletionEventListener

---
+++ src/EventListener/TaskReportedCompletionEventListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Event\AbstractTaskReportCompletionEvent;
+use App\Event\TaskReportCompletionFailureEvent;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+
+class TaskReportedCompletionEventListener
+{
+    private $entityManager;
+    private $logger;
+
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)
+    {
+        $this->entityManager = $entityManager;
+        $this->logger = $logger;
+    }
+
+    public function __invoke(AbstractTaskReportCompletionEvent $taskReportCompletionEvent)
+    {
+        $task = $taskReportCompletionEvent->getTask();
+
+        if ($taskReportCompletionEvent->isSucceeded()) {
+            $this->entityManager->remove($task);
+            $this->entityManager->remove($task->getOutput());
+            $this->entityManager->remove($task->getTimePeriod());
+            $this->entityManager->flush();
+        } else {
+            /* @var TaskReportCompletionFailureEvent $taskReportCompletionEvent */
+            $this->logger->error(
+                'task-report-completion failed: [' . $task->getId() . ']',
+                [
+                    'request_url' => $taskReportCompletionEvent->getRequestUrl(),
+                    'failure_type' => $taskReportCompletionEvent->getFailureType(),
+                    'status_code' => $taskReportCompletionEvent->getStatusCode(),
+                ]
+            );
+        }
+    }
+}
