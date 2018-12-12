a2f3717be23addcfbb4b3bab81f748623250d0a8
Jon Cram <webignition@gmail.com>
2018-12-12 16:38:13 +0000

    Add CachedResource::contentType (#226)

--- tests/Functional/Entity/CachedResourceTest.php
+++ tests/Functional/Entity/CachedResourceTest.php
@@ -24,16 +24,19 @@ class CachedResourceTest extends AbstractBaseTestCase
      * @dataProvider createDataProvider
      *
      * @param string $url
+     * @param string $contentType
      * @param string $body
      */
-    public function testCreate(string $url, string $body)
+    public function testCreate(string $url, string $contentType, string $body)
     {
         $resource = new CachedResource();
 
         $resource->setUrl($url);
+        $resource->setContentType($contentType);
         $resource->setBody($body);
 
         $this->assertEquals($url, $resource->getUrl());
+        $this->assertEquals($contentType, $resource->getContentType());
         $this->assertEquals($body, stream_get_contents($resource->getBody()));
 
         $this->entityManager->persist($resource);
@@ -47,6 +50,7 @@ class CachedResourceTest extends AbstractBaseTestCase
         $retrievedResource = $this->entityManager->find(CachedResource::class, $id);
 
         $this->assertEquals($url, $retrievedResource->getUrl());
+        $this->assertEquals($contentType, $retrievedResource->getContentType());
         $this->assertEquals($body, stream_get_contents($retrievedResource->getBody()));
     }
 
@@ -55,14 +59,17 @@ class CachedResourceTest extends AbstractBaseTestCase
         return [
             'empty body' => [
                 'url' => 'http://example.com/',
+                'contentType' => '',
                 'body' => '',
             ],
             'has body' => [
                 'url' => 'http://example.com/',
+                'contentType' => 'text/plain',
                 'body' => 'body content',
             ],
             'has mb body' => [
                 'url' => 'http://example.com/',
+                'contentType' => 'text/plain',
                 'body' => '내 호버크라프트는 뱀장어로 가득하다',
             ],
         ];
