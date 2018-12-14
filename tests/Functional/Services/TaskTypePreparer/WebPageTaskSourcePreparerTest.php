50487af44230820a4d41513e048fbb0f748af9df
Jon Cram <jon@webignition.net>
2018-12-14 19:20:12 +0000

    Verify success with no pre-existing cached resource

--- tests/Functional/Services/TaskTypePreparer/WebPageTaskSourcePreparerTest.php
+++ tests/Functional/Services/TaskTypePreparer/WebPageTaskSourcePreparerTest.php
@@ -2,14 +2,15 @@
 
 namespace App\Tests\Functional\Services\TaskTypePreparer;
 
+use App\Entity\CachedResource;
 use App\Entity\Task\Task;
-use App\Model\Source;
 use App\Model\Task\Type;
 use App\Services\SourceFactory;
 use App\Services\TaskTypePreparer\WebPageTaskSourcePreparer;
 use App\Services\TaskTypeService;
 use App\Tests\Functional\AbstractBaseTestCase;
 use App\Tests\Services\HttpMockHandler;
+use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Psr7\Response;
 
 class WebPageTaskSourcePreparerTest extends AbstractBaseTestCase
@@ -81,5 +82,40 @@ class WebPageTaskSourcePreparerTest extends AbstractBaseTestCase
                 'contentType' => 'f o o',
             ],
         ];
+    }
+
+    public function testPrepareSuccessNoPreExistingCachedResource()
+    {
+        $entityManager = self::$container->get(EntityManagerInterface::class);
+        $cachedResourceRepository = $entityManager->getRepository(CachedResource::class);
+
+        $this->httpMockHandler->appendFixtures([
+            new Response(200, ['content-type' => 'text/html']),
+            new Response(200, ['content-type' => 'text/html'], 'html content'),
+        ]);
+
+        $taskTypeService = self::$container->get(TaskTypeService::class);
+
+        $url = 'http://example.com';
+        $task = Task::create($taskTypeService->get(Type::TYPE_HTML_VALIDATION), $url);
+
+        $this->assertEquals([], $task->getSources());
+        $this->assertEquals([], $cachedResourceRepository->findAll());
+
+        $this->preparer->prepare($task);
+
+        /* @var CachedResource $cachedResource */
+        $cachedResource = $cachedResourceRepository->findOneBy([
+            'url' => $url,
+        ]);
+
+        $expectedSource = $this->sourceFactory->fromCachedResource($cachedResource);
+
+        $this->assertEquals(
+            [
+                $url => $expectedSource,
+            ],
+            $task->getSources()
+        );
     }
 }
