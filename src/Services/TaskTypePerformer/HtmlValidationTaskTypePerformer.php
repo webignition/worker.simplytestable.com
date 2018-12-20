3b33e4dcd9a1b86105104936b6af769b3e05c623
Jon Cram <jon@webignition.net>
2018-12-20 14:57:43 +0000

    Re-implement HtmlValidationTaskTypePerformer

--- src/Services/TaskTypePerformer/HtmlValidationTaskTypePerformer.php
+++ src/Services/TaskTypePerformer/HtmlValidationTaskTypePerformer.php
@@ -2,21 +2,71 @@
 
 namespace App\Services\TaskTypePerformer;
 
+use App\Entity\Task\Output as TaskOutput;
+use App\Entity\Task\Task;
+use App\Model\TaskTypePerformer\Response as TaskTypePerformerResponse;
 use App\Services\HttpClientConfigurationService;
 use App\Services\HttpClientService;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\ResponseInterface;
 use webignition\HtmlValidator\Output\Parser\Configuration as HtmlValidatorOutputParserConfiguration;
 use webignition\HtmlValidator\Wrapper\Wrapper as HtmlValidatorWrapper;
 use webignition\InternetMediaType\InternetMediaType;
+use webignition\InternetMediaType\Parser\ParseException as InternetMediaTypeParseException;
+use webignition\WebResource\Exception\HttpException;
+use webignition\WebResource\Exception\InvalidResponseContentTypeException;
+use webignition\WebResource\Exception\TransportException;
 use webignition\WebResource\Retriever as WebResourceRetriever;
 use webignition\WebResource\WebPage\WebPage;
 use webignition\HtmlDocumentType\Extractor as DoctypeExtractor;
 use webignition\HtmlDocumentType\Validator as DoctypeValidator;
 use webignition\HtmlDocumentType\Factory as DoctypeFactory;
 use webignition\HttpHistoryContainer\Container as HttpHistoryContainer;
+use webignition\WebResourceInterfaces\WebResourceInterface;
 
-class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
+class HtmlValidationTaskTypePerformer implements TaskTypePerformerInterface
 {
     const DEFAULT_CHARACTER_ENCODING = 'UTF-8';
+    const USER_AGENT = 'ST Web Resource Task Driver (http://bit.ly/RlhKCL)';
+
+    const CURL_CODE_INVALID_URL = 3;
+    const CURL_CODE_TIMEOUT = 28;
+    const CURL_CODE_DNS_LOOKUP_FAILURE = 6;
+
+    /**
+     * @var HttpException
+     */
+    private $httpException;
+
+    /**
+     * @var TransportException
+     */
+    private $transportException;
+
+    /**
+     * @var Task
+     */
+    protected $task;
+
+    /**
+     * @var WebResourceRetriever
+     */
+    private $webResourceRetriever;
+
+    /**
+     * @var HttpClientService
+     */
+    protected $httpClientService;
+
+    /**
+     * @var HttpClientConfigurationService
+     */
+    private $httpClientConfigurationService;
+
+    /**
+     * @var HttpHistoryContainer
+     */
+    private $httpHistoryContainer;
 
     /**
      * @var HtmlValidatorWrapper
@@ -28,6 +78,11 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
      */
     private $validatorPath;
 
+    /**
+     * @var TaskTypePerformerResponse
+     */
+    protected $response = null;
+
     public function __construct(
         HttpClientService $httpClientService,
         HttpClientConfigurationService $httpClientConfigurationService,
@@ -36,21 +91,110 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
         HtmlValidatorWrapper $htmlValidatorWrapper,
         string $validatorPath
     ) {
-        parent::__construct(
-            $httpClientService,
-            $httpClientConfigurationService,
-            $webResourceRetriever,
-            $httpHistoryContainer
-        );
+        $this->httpClientService = $httpClientService;
+        $this->httpClientConfigurationService = $httpClientConfigurationService;
+        $this->webResourceRetriever = $webResourceRetriever;
+        $this->httpHistoryContainer = $httpHistoryContainer;
 
         $this->htmlValidatorWrapper = $htmlValidatorWrapper;
         $this->validatorPath = $validatorPath;
     }
 
     /**
+     * @param Task $task
+     *
+     * @return TaskTypePerformerResponse
+     * @throws InternetMediaTypeParseException
+     * @throws TransportException
+     */
+    public function perform(Task $task): TaskTypePerformerResponse
+    {
+        $this->response = new TaskTypePerformerResponse();
+
+        $rawOutput = $this->execute($task);
+
+        $this->response->setTaskOutput(TaskOutput::create(
+            $rawOutput,
+            $this->getOutputContentType(),
+            $this->response->getErrorCount(),
+            $this->response->getWarningCount()
+        ));
+
+        return $this->response;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws InternetMediaTypeParseException
+     * @throws TransportException
+     */
+    private function execute(Task $task)
+    {
+        $this->task = $task;
+        $this->httpClientConfigurationService->configureForTask($task, self::USER_AGENT);
+
+        $webPage = $this->retrieveWebPage();
+
+        if (!$this->response->hasSucceeded()) {
+            return $this->hasNotSucceededHandler();
+        }
+
+        if (!$webPage instanceof WebPage) {
+            $this->response->setHasBeenSkipped();
+            $this->response->setIsRetryable(false);
+            $this->response->setErrorCount(0);
+
+            return null;
+        }
+
+        if (empty($webPage->getContent())) {
+            $this->response->setHasBeenSkipped();
+            $this->response->setErrorCount(0);
+
+            return null;
+        }
+
+        return $this->performValidation($webPage);
+    }
+
+    /**
+     * @return WebResourceInterface
+     *
+     * @throws InternetMediaTypeParseException
+     * @throws TransportException
+     */
+    private function retrieveWebPage()
+    {
+        $request = new Request('GET', $this->task->getUrl());
+
+        try {
+            return $this->webResourceRetriever->retrieve($request);
+        } catch (InvalidResponseContentTypeException $invalidResponseContentTypeException) {
+            $this->response->setHasBeenSkipped();
+            $this->response->setIsRetryable(false);
+            $this->response->setErrorCount(0);
+        } catch (HttpException $httpException) {
+            $this->httpException = $httpException;
+            $this->response->setHasFailed();
+            $this->response->setIsRetryable(false);
+        } catch (TransportException $transportException) {
+            if (!$transportException->isCurlException() && !$transportException->isTooManyRedirectsException()) {
+                throw $transportException;
+            }
+
+            $this->transportException = $transportException;
+            $this->response->setHasFailed();
+            $this->response->setIsRetryable(false);
+        }
+
+        return null;
+    }
+
+    /**
      * {@inheritdoc}
      */
-    protected function hasNotSucceededHandler()
+    private function hasNotSucceededHandler()
     {
         $this->response->setErrorCount(1);
 
@@ -58,18 +202,25 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
     }
 
     /**
-     * {@inheritdoc}
+     * @return \stdClass
      */
-    protected function isBlankWebResourceHandler()
+    private function getHttpExceptionOutput()
     {
-        $this->response->setHasBeenSkipped();
-        $this->response->setErrorCount(0);
+        return (object)[
+            'messages' => [
+                [
+                    'message' => $this->getOutputMessage(),
+                    'messageId' => 'http-retrieval-' . $this->getOutputMessageId(),
+                    'type' => 'error',
+                ]
+            ],
+        ];
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function performValidation(WebPage $webPage)
+    private function performValidation(WebPage $webPage)
     {
         $webPageContent = $webPage->getContent();
         $docTypeString = DoctypeExtractor::extract($webPageContent);
@@ -172,7 +323,7 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
     /**
      * @return \stdClass
      */
-    protected function getMissingDocumentTypeOutput()
+    private function getMissingDocumentTypeOutput()
     {
         return (object)[
             'messages' => [
@@ -190,7 +341,7 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
      *
      * @return \stdClass
      */
-    protected function getIsNotMarkupOutput($fragment)
+    private function getIsNotMarkupOutput($fragment)
     {
         return (object)[
             'messages' => [
@@ -209,7 +360,7 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
      *
      * @return \stdClass
      */
-    protected function getInvalidDocumentTypeOutput($documentType)
+    private function getInvalidDocumentTypeOutput($documentType)
     {
         return (object)[
             'messages' => [
@@ -226,12 +377,117 @@ class HtmlValidationTaskTypePerformer extends AbstractWebPageTaskTypePerformer
      *
      * @return InternetMediaType
      */
-    protected function getOutputContentType()
+    private function getOutputContentType()
     {
         $contentType = new InternetMediaType();
         $contentType->setType('application');
         $contentType->setSubtype('json');
 
         return $contentType;
+    }
+
+    /**
+     * @return string
+     */
+    private function getOutputMessage()
+    {
+        if ($this->hasTooManyRedirectsException()) {
+            if ($this->isRedirectLoopException()) {
+                return 'Redirect loop detected';
+            }
+
+            return 'Redirect limit reached';
+        }
+
+        if (!empty($this->transportException)) {
+            if ($this->transportException->isCurlException()) {
+                if (self::CURL_CODE_TIMEOUT == $this->transportException->getCode()) {
+                    return 'Timeout reached retrieving resource';
+                }
+
+                if (self::CURL_CODE_DNS_LOOKUP_FAILURE == $this->transportException->getCode()) {
+                    return 'DNS lookup failure resolving resource domain name';
+                }
+
+                if (self::CURL_CODE_INVALID_URL == $this->transportException->getCode()) {
+                    return 'Invalid resource URL';
+                }
+
+                return '';
+            }
+
+            return '';
+        }
+
+        return $this->httpException->getMessage();
+    }
+
+    /**
+     * @return string
+     */
+    private function getOutputMessageId()
+    {
+        if ($this->hasTooManyRedirectsException()) {
+            if ($this->isRedirectLoopException()) {
+                return 'redirect-loop';
+            }
+
+            return 'redirect-limit-reached';
+        }
+
+        if (!empty($this->transportException)) {
+            return 'curl-code-' . $this->transportException->getCode();
+        }
+
+        if (!empty($this->curlException)) {
+            return 'curl-code-' . $this->curlException->getCurlCode();
+        }
+
+        return $this->httpException->getCode();
+    }
+
+    /**
+     * @return boolean
+     */
+    private function isRedirectLoopException()
+    {
+        /* @var ResponseInterface[] $responses */
+        $responses = $this->httpHistoryContainer->getResponses();
+        $responseHistoryContainsOnlyRedirects = true;
+
+        foreach ($responses as $response) {
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 300 || $statusCode >=400) {
+                $responseHistoryContainsOnlyRedirects = false;
+            }
+        }
+
+        if (!$responseHistoryContainsOnlyRedirects) {
+            return false;
+        }
+
+        $requestUrls = $this->httpHistoryContainer->getRequestUrlsAsStrings();
+        $requestUrls = array_slice($requestUrls, count($requestUrls) / 2);
+
+        foreach ($requestUrls as $urlIndex => $url) {
+            if (in_array($url, array_slice($requestUrls, $urlIndex + 1))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasTooManyRedirectsException()
+    {
+        if (empty($this->transportException)) {
+            return false;
+        }
+
+        return $this->transportException->isTooManyRedirectsException();
     }
 }
