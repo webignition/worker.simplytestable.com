7f99b45b05b8f17914c63ef58971f8e6fbfdd8d0
Jon Cram <jon@webignition.net>
2018-12-20 14:11:51 +0000

    Fix interface

--- src/Services/TaskTypePerformer/TaskPerformerInterface.php
+++ src/Services/TaskTypePerformer/TaskPerformerInterface.php
@@ -6,7 +6,7 @@ use App\Entity\Task\Task;
 
 interface TaskPerformerInterface
 {
-    public function prepare(Task $task): bool;
+    public function perform(Task $task): bool;
     public function handles(string $taskType): bool;
     public function getPriority(): int;
 }
