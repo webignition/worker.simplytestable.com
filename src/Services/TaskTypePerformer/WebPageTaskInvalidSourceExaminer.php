aaf641d79c995b50d8a6a4866e468648e543807d
Jon Cram <jon@webignition.net>
2018-12-21 18:54:25 +0000

    Skip tasks where source is not valid

---
+++ src/Services/TaskTypePerformer/WebPageTaskInvalidSourceExaminer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services\TaskTypePerformer;
+
+use App\Entity\CachedResource;
+use App\Entity\Task\Output;
+use App\Entity\Task\Task;
+use App\Model\Source;
+use App\Model\Task\TypeInterface;
+use App\Services\CachedResourceManager;
+use webignition\InternetMediaType\InternetMediaType;
+
+class WebPageTaskInvalidSourceExaminer implements TaskPerformerInterface
+{
+    private $cachedResourceManager;
+    private $priority;
+
+    public function __construct(CachedResourceManager $cachedResourceManager, int $priority)
+    {
+        $this->cachedResourceManager = $cachedResourceManager;
+        $this->priority = $priority;
+    }
+
+    public function perform(Task $task)
+    {
+        $sources = $task->getSources();
+        /* @var Source $primarySource */
+        $primarySource = $sources[$task->getUrl()] ?? null;
+
+        if (empty($primarySource)) {
+            return;
+        }
+
+        if ($primarySource->isCachedResource()) {
+            /* @var CachedResource $cachedResource */
+            $cachedResource = $this->cachedResourceManager->find($primarySource->getValue());
+
+            if ($cachedResource && '' === stream_get_contents($cachedResource->getBody())) {
+                $this->setTaskAsSkipped($task);
+            }
+        } else {
+            if ($primarySource->isInvalidContentType()) {
+                $this->setTaskAsSkipped($task);
+            }
+        }
+    }
+
+    private function setTaskAsSkipped(Task $task)
+    {
+        $task->setState(Task::STATE_SKIPPED);
+        $task->setOutput(Output::create('', new InternetMediaType('application', 'json')));
+    }
+
+    public function handles(string $taskType): bool
+    {
+        return in_array($taskType, [
+            TypeInterface::TYPE_HTML_VALIDATION,
+            TypeInterface::TYPE_CSS_VALIDATION,
+            TypeInterface::TYPE_LINK_INTEGRITY,
+            TypeInterface::TYPE_LINK_INTEGRITY_SINGLE_URL,
+            TypeInterface::TYPE_URL_DISCOVERY,
+        ]);
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+}
---
+++ tests/Functional/Services/TaskTypePerformer/WebPageTaskInvalidSourceExaminerTest.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Tests\Functional\Services\TaskTypePerformer;
+
+use App\Entity\Task\Output;
+use App\Entity\Task\Task;
+use App\Model\Source;
+use App\Model\Task\TypeInterface;
+use App\Services\CachedResourceFactory;
+use App\Services\CachedResourceManager;
+use App\Services\SourceFactory;
+use App\Services\TaskTypePerformer\WebPageTaskInvalidSourceExaminer;
+use App\Tests\Functional\AbstractBaseTestCase;
+use App\Tests\Services\TestTaskFactory;
+use webignition\WebResource\WebPage\WebPage;
+
+class WebPageTaskInvalidSourceExaminerTest extends AbstractBaseTestCase
+{
+    /**
+     * @var WebPageTaskInvalidSourceExaminer
+     */
+    private $examiner;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->examiner = self::$container->get(WebPageTaskInvalidSourceExaminer::class);
+    }
+
+    /**
+     * @dataProvider performNoChangesDataProvider
+     *
+     * @param callable $taskCreator
+     */
+    public function testPerformNoChanges(callable $taskCreator)
+    {
+        $testTaskFactory = self::$container->get(TestTaskFactory::class);
+        $sourceFactory = self::$container->get(SourceFactory::class);
+        $cachedResourceFactory = self::$container->get(CachedResourceFactory::class);
+        $cachedResourceManager = self::$container->get(CachedResourceManager::class);
+
+        /* @var Task $task */
+        $task = $taskCreator($testTaskFactory, $sourceFactory, $cachedResourceFactory, $cachedResourceManager);
+
+        $taskState = $task->getState();
+
+        $this->examiner->perform($task);
+
+        $this->assertEquals($taskState, $task->getState());
+        $this->assertNull($task->getOutput());
+    }
+
+    public function performNoChangesDataProvider()
+    {
+        return [
+            'no sources' => [
+                'taskCreator' => function (TestTaskFactory $testTaskFactory): Task {
+                    return $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+                },
+            ],
+            'no primary source' => [
+                'taskCreator' => function (TestTaskFactory $testTaskFactory, SourceFactory $sourceFactory): Task {
+                    $task = $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+
+                    $task->addSource(
+                        $sourceFactory->createHttpFailedSource('http://example.com/404', 404)
+                    );
+
+                    return $task;
+                },
+            ],
+            'invalid primary source, not invalid content type' => [
+                'taskCreator' => function (TestTaskFactory $testTaskFactory, SourceFactory $sourceFactory): Task {
+                    $task = $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+
+                    $task->addSource(
+                        $sourceFactory->createHttpFailedSource($task->getUrl(), 404)
+                    );
+
+                    return $task;
+                },
+            ],
+            'non-empty cached resource' => [
+                'taskCreator' => function (
+                    TestTaskFactory $testTaskFactory,
+                    SourceFactory $sourceFactory,
+                    CachedResourceFactory $cachedResourceFactory,
+                    CachedResourceManager $cachedResourceManager
+                ): Task {
+                    $task = $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+
+                    /* @var WebPage $webPage */
+                    /** @noinspection PhpUnhandledExceptionInspection */
+                    $webPage = WebPage::createFromContent('non-empty content');
+
+                    $cachedResource = $cachedResourceFactory->createForTask('request_hash', $task, $webPage);
+                    $cachedResourceManager->persist($cachedResource);
+
+                    $task->addSource(
+                        $sourceFactory->fromCachedResource($cachedResource)
+                    );
+
+                    return $task;
+                },
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider performSetsTaskAsSkippedDataProvider
+     *
+     * @param callable $taskCreator
+     */
+    public function testPerformSetsTaskAsSkipped(callable $taskCreator)
+    {
+        $testTaskFactory = self::$container->get(TestTaskFactory::class);
+        $sourceFactory = self::$container->get(SourceFactory::class);
+        $cachedResourceFactory = self::$container->get(CachedResourceFactory::class);
+        $cachedResourceManager = self::$container->get(CachedResourceManager::class);
+
+        /* @var Task $task */
+        $task = $taskCreator($testTaskFactory, $sourceFactory, $cachedResourceFactory, $cachedResourceManager);
+
+        $this->assertEquals(Task::STATE_QUEUED, $task->getState());
+        $this->assertNull($task->getOutput());
+
+        $this->examiner->perform($task);
+
+        $this->assertEquals(Task::STATE_SKIPPED, $task->getState());
+
+        $taskOutput = $task->getOutput();
+        $this->assertInstanceOf(Output::class, $taskOutput);
+        $this->assertEquals(0, $taskOutput->getErrorCount());
+        $this->assertEquals(0, $taskOutput->getWarningCount());
+        $this->assertEquals('', $taskOutput->getOutput());
+    }
+
+    public function performSetsTaskAsSkippedDataProvider()
+    {
+        return [
+            'invalid primary source, is invalid content type' => [
+                'taskCreator' => function (TestTaskFactory $testTaskFactory, SourceFactory $sourceFactory): Task {
+                    $task = $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+
+                    $source = $sourceFactory->createInvalidSource(
+                        $task->getUrl(),
+                        Source::MESSAGE_INVALID_CONTENT_TYPE
+                    );
+
+                    $task->addSource($source);
+
+                    return $task;
+                },
+            ],
+            'empty cached resource' => [
+                'taskCreator' => function (
+                    TestTaskFactory $testTaskFactory,
+                    SourceFactory $sourceFactory,
+                    CachedResourceFactory $cachedResourceFactory,
+                    CachedResourceManager $cachedResourceManager
+                ): Task {
+                    $task = $testTaskFactory->create(
+                        TestTaskFactory::createTaskValuesFromDefaults()
+                    );
+
+                    /* @var WebPage $webPage */
+                    /** @noinspection PhpUnhandledExceptionInspection */
+                    $webPage = WebPage::createFromContent('');
+
+                    $cachedResource = $cachedResourceFactory->createForTask('request_hash', $task, $webPage);
+                    $cachedResourceManager->persist($cachedResource);
+
+                    $task->addSource(
+                        $sourceFactory->fromCachedResource($cachedResource)
+                    );
+
+                    return $task;
+                },
+            ],
+        ];
+    }
+
+    public function testHandles()
+    {
+        $this->assertTrue($this->examiner->handles(TypeInterface::TYPE_HTML_VALIDATION));
+        $this->assertTrue($this->examiner->handles(TypeInterface::TYPE_CSS_VALIDATION));
+        $this->assertTrue($this->examiner->handles(TypeInterface::TYPE_LINK_INTEGRITY));
+        $this->assertTrue($this->examiner->handles(TypeInterface::TYPE_LINK_INTEGRITY_SINGLE_URL));
+        $this->assertTrue($this->examiner->handles(TypeInterface::TYPE_URL_DISCOVERY));
+    }
+
+    public function testGetPriority()
+    {
+        $this->assertEquals(
+            self::$container->getParameter('web_page_task_invalid_source_examiner_priority'),
+            $this->examiner->getPriority()
+        );
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        \Mockery::close();
+    }
+}
