252097e4de5441b8b2a2a530501cd8eac1d3051b
Jon Cram <jon@webignition.net>
2019-02-07 10:39:47 +0000

    Add CssSourceInspector::findImportValues()

--- src/Services/CssSourceInspector.php
+++ src/Services/CssSourceInspector.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use Sabberworm\CSS\Parser as CssParser;
+use Sabberworm\CSS\Property\Charset;
+use Sabberworm\CSS\Property\Import;
 use webignition\WebResource\WebPage\WebPage;
 
 class CssSourceInspector
@@ -29,5 +32,38 @@ class CssSourceInspector
         }
 
         return $styleBlocks;
+    }
+
+    /**
+     * @param string $css
+     *
+     * @return string[]
+     */
+    public function findImportValues(string $css): array
+    {
+        $importValues = [];
+
+        $cssParser = new CssParser($css);
+
+        $cssDocument = $cssParser->parse();
+        $cssContents = $cssDocument->getContents();
+
+        $nonImportFound = false;
+
+        foreach ($cssContents as $item) {
+            if ($item instanceof Import && !$nonImportFound) {
+                $value = $item->getLocation()->getURL()->getString();
+
+                if (!in_array($value, $importValues)) {
+                    $importValues[] = $value;
+                }
+            }
+
+            if (!$item instanceof Import && !$item instanceof Charset) {
+                $nonImportFound = true;
+            }
+        }
+
+        return $importValues;
     }
 }
--- tests/Unit/Services/CssSourceInspectorTest.php
+++ tests/Unit/Services/CssSourceInspectorTest.php
@@ -58,4 +58,102 @@ class CssSourceInspectorTest extends \PHPUnit\Framework\TestCase
             ],
         ];
     }
+
+    /**
+     * @dataProvider findImportValuesDataProvider
+     */
+    public function testFindImportValues(string $css, array $expectedImportValues)
+    {
+        $importValues = $this->cssSourceInspector->findImportValues($css);
+
+        $this->assertEquals($expectedImportValues, $importValues);
+    }
+
+    public function findImportValuesDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'css' => '',
+                'expectedImportValues' => [],
+            ],
+            'no import values' => [
+                'css' => 'html {}',
+                'expectedImportValues' => [],
+            ],
+            'single import (url, double-quoted)' => [
+                'css' => '@import url("style.css");',
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'single import (url, single-quoted)' => [
+                'css' => "@import url('style.css');",
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'single import (url, unquoted)' => [
+                'css' => '@import url(style.css);',
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'single import (string, double-quoted)' => [
+                'css' => '@import "style.css";',
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'single import (string single-quoted)' => [
+                'css' => "@import 'style.css';",
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'charset, import' => [
+                'css' => '@charset "utf-8";@import url("style.css");',
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'import, import' => [
+                'css' => '@import url("one.css");@import url("two.css");',
+                'expectedImportValues' => [
+                    'one.css',
+                    'two.css',
+                ],
+            ],
+            'duplicated import' => [
+                'css' => '@import url("style.css");@import url("style.css");',
+                'expectedImportValues' => [
+                    'style.css',
+                ],
+            ],
+            'import, css, import' => [
+                'css' => implode("\n", [
+                    '@import url("one.css");',
+                    'html {}',
+                    '@import url("two.css");',
+                ]),
+                'expectedImportValues' => [
+                    'one.css',
+                ],
+            ],
+            'import, charset, import (charset incorrectly ignored by CSS parser)' => [
+                'css' => implode("\n", [
+                    '@import "one.css";',
+                    '@charset "utf-8";',
+                    '@import "two.css";',
+                ]),
+                'expectedImportValues' => [
+                    'one.css',
+                    'two.css',
+                ],
+            ],
+        ];
+    }
 }
+
+
+
+
