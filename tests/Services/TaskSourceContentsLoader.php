d0aa4d5b09fcf618030aa432d1523361caca7f31
Jon Cram <webignition@gmail.com>
2019-02-11 18:02:34 +0000

    Add more integration tests for CSS validation (#428)

---
+++ tests/Services/TaskSourceContentsLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Services;
+
+use App\Entity\CachedResource;
+use App\Model\Source;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TaskSourceContentsLoader
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Source[] $sources
+     *
+     * @return string[]
+     */
+    public function load(array $sources): array
+    {
+        $contents = [];
+
+        foreach ($sources as $source) {
+            /* @var CachedResource $cachedResource */
+            $cachedResource = $this->entityManager->find(CachedResource::class, $source->getValue());
+            $contents[$source->getUrl()] = stream_get_contents($cachedResource->getBody());
+        }
+
+        return $contents;
+    }
+}
