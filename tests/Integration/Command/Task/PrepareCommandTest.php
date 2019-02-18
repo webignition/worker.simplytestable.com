fa90c071fcdc1f61317c52a96feefc6441e7c203
Jon Cram <jon@webignition.net>
2019-02-18 17:28:23 +0000

    Handle task completing during preparation

--- tests/Integration/Command/Task/PrepareCommandTest.php
+++ tests/Integration/Command/Task/PrepareCommandTest.php
@@ -5,9 +5,12 @@
 namespace App\Tests\Integration\Command\Task;
 
 use App\Command\Task\PrepareCommand;
+use App\Entity\CachedResource;
+use App\Entity\Task\Output;
 use App\Entity\Task\Task;
 use App\Model\Source;
 use App\Model\Task\TypeInterface;
+use App\Resque\Job\TaskReportCompletionJob;
 use App\Tests\Factory\HtmlDocumentFactory;
 use App\Tests\Services\HttpMockHandler;
 use App\Tests\Services\TaskSourceContentsLoader;
@@ -18,6 +21,7 @@ use GuzzleHttp\Psr7\Response;
 use Symfony\Component\Console\Output\NullOutput;
 use App\Tests\Functional\AbstractBaseTestCase;
 use Symfony\Component\Console\Input\ArrayInput;
+use webignition\InternetMediaType\InternetMediaType;
 
 /**
  * @group Command/Task/PrepareCommand
@@ -49,9 +53,9 @@ class PrepareCommandTest extends AbstractBaseTestCase
     }
 
     /**
-     * @dataProvider runDataProvider
+     * @dataProvider runTaskPreparedDataProvider
      */
-    public function testRun(
+    public function testRunTaskPrepared(
         array $httpFixtures,
         array $taskValues,
         array $expectedSources,
@@ -82,6 +86,8 @@ class PrepareCommandTest extends AbstractBaseTestCase
         $sourceContents = $this->taskSourceContentsLoader->load($sources);
         $this->assertEquals($expectedSourceContents, $sourceContents);
 
+        $this->assertEquals(Task::STATE_PREPARED, $task->getState());
+
         $this->assertTrue(self::$container->get(QueueService::class)->contains(
             'task-perform',
             [
@@ -90,7 +96,7 @@ class PrepareCommandTest extends AbstractBaseTestCase
         ));
     }
 
-    public function runDataProvider(): array
+    public function runTaskPreparedDataProvider(): array
     {
         return [
             'html validation' => [
@@ -206,6 +212,164 @@ class PrepareCommandTest extends AbstractBaseTestCase
                 ],
                 'expectedSourceContents' => [
                     'http://example.com/' => '<doctype html>',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider runTaskCompletedDataProvider
+     */
+    public function testRunTaskCompleted(
+        array $httpFixtures,
+        array $taskValues,
+        bool $expectedHasPrimarySource,
+        string $expectedTaskState,
+        int $expectedErrorCount,
+        $expectedDecodedOutput
+    ) {
+        $testTaskFactory = self::$container->get(TestTaskFactory::class);
+        $httpMockHandler = self::$container->get(HttpMockHandler::class);
+        $entityManager = self::$container->get(EntityManagerInterface::class);
+
+        $httpMockHandler->appendFixtures($httpFixtures);
+
+        $task = $testTaskFactory->create($taskValues);
+
+        $sources = $task->getSources();
+        $primarySource = $sources[$task->getUrl()];
+        $primarySourceRequestHash = $primarySource->getValue();
+
+        $primaryCachedResource = $entityManager->find(CachedResource::class, $primarySourceRequestHash);
+
+        if ($expectedHasPrimarySource) {
+            $this->assertNotNull($primaryCachedResource);
+        } else {
+            $this->assertNull($primaryCachedResource);
+        }
+
+        $returnCode = $this->command->run(
+            new ArrayInput([
+                'id' => $task->getId(),
+            ]),
+            new NullOutput()
+        );
+
+        $this->assertEquals(0, $returnCode);
+        $this->assertEquals($expectedTaskState, $task->getState());
+
+        $output = $task->getOutput();
+
+        $this->assertInstanceOf(Output::class, $output);
+        $this->assertEquals($expectedErrorCount, $output->getErrorCount());
+        $this->assertEquals(0, $output->getWarningCount());
+        $this->assertEquals('application/json', $output->getContentType());
+        $this->assertEquals($expectedDecodedOutput, json_decode($output->getOutput(), true));
+
+        $this->assertTrue(self::$container->get(QueueService::class)->contains(
+            TaskReportCompletionJob::QUEUE_NAME,
+            [
+                'id' => $task->getId()
+            ]
+        ));
+
+        $this->assertNull($entityManager->find(CachedResource::class, $primarySourceRequestHash));
+    }
+
+    public function runTaskCompletedDataProvider(): array
+    {
+        return [
+            'html validation, invalid character encoding' => [
+                'httpFixtures' => [],
+                'taskValues' => TestTaskFactory::createTaskValuesFromDefaults([
+                    'url' => 'http://example.com/',
+                    'type' => TypeInterface::TYPE_HTML_VALIDATION,
+                    'sources' => [
+                        [
+                            'type' => Source::TYPE_CACHED_RESOURCE,
+                            'url' => 'http://example.com/',
+                            'content' => "\xc3\x28",
+                            'contentType' => new InternetMediaType('text', 'html'),
+                        ],
+                    ],
+                ]),
+                'expectedHasPrimarySource' => true,
+                'expectedTaskState' => Task::STATE_FAILED_NO_RETRY_AVAILABLE,
+                'expectedErrorCount' => 1,
+                'expectedDecodedOutput' => [
+                    'messages' => [
+                        [
+                            'message' => 'utf-8',
+                            'messageId' => 'invalid-character-encoding',
+                            'type' => 'error',
+                        ],
+                    ],
+                ],
+            ],
+            'html validation, empty primary source' => [
+                'httpFixtures' => [],
+                'taskValues' => TestTaskFactory::createTaskValuesFromDefaults([
+                    'url' => 'http://example.com/',
+                    'type' => TypeInterface::TYPE_HTML_VALIDATION,
+                    'sources' => [
+                        [
+                            'type' => Source::TYPE_CACHED_RESOURCE,
+                            'url' => 'http://example.com/',
+                            'content' => '',
+                            'contentType' => new InternetMediaType('text', 'html'),
+                        ],
+                    ],
+                ]),
+                'expectedHasPrimarySource' => true,
+                'expectedTaskState' => Task::STATE_SKIPPED,
+                'expectedErrorCount' => 0,
+                'expectedDecodedOutput' => null,
+            ],
+            'html validation, invalid content type primary source' => [
+                'httpFixtures' => [],
+                'taskValues' => TestTaskFactory::createTaskValuesFromDefaults([
+                    'url' => 'http://example.com/',
+                    'type' => TypeInterface::TYPE_HTML_VALIDATION,
+                    'sources' => [
+                        [
+                            'type' => Source::TYPE_INVALID,
+                            'url' => 'http://example.com/',
+                            'value' => 'invalid:' . sprintf(
+                                Source::MESSAGE_INVALID_CONTENT_TYPE,
+                                'text/html'
+                            ),
+                        ],
+                    ],
+                ]),
+                'expectedHasPrimarySource' => false,
+                'expectedTaskState' => Task::STATE_SKIPPED,
+                'expectedErrorCount' => 0,
+                'expectedDecodedOutput' => null,
+            ],
+            'html validation, failed primary source' => [
+                'httpFixtures' => [],
+                'taskValues' => TestTaskFactory::createTaskValuesFromDefaults([
+                    'url' => 'http://example.com/',
+                    'type' => TypeInterface::TYPE_HTML_VALIDATION,
+                    'sources' => [
+                        [
+                            'type' => Source::TYPE_UNAVAILABLE,
+                            'url' => 'http://example.com/',
+                            'value' => 'http:404',
+                        ],
+                    ],
+                ]),
+                'expectedHasPrimarySource' => false,
+                'expectedTaskState' => Task::STATE_FAILED_NO_RETRY_AVAILABLE,
+                'expectedErrorCount' => 1,
+                'expectedDecodedOutput' => [
+                    'messages' => [
+                        [
+                            'message' => '',
+                            'messageId' => 'http-retrieval-404',
+                            'type' => 'error',
+                        ],
+                    ],
                 ],
             ],
         ];
