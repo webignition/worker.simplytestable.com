d4d92be213caabe9b9d4aa1a1cca8acd356c9e94
Jon Cram <webignition@gmail.com>
2019-03-25 17:24:27 +0000

    phpstan level 5 for tests (#514)

--- src/Request/Task/CancelRequest.php
+++ src/Request/Task/CancelRequest.php
@@ -6,31 +6,19 @@ use App\Entity\Task\Task;
 
 class CancelRequest
 {
-    /**
-     * @var Task
-     */
     private $task;
 
-    /**
-     * @param Task $task
-     */
-    public function __construct($task)
+    public function __construct(?Task $task)
     {
         $this->task = $task;
     }
 
-    /**
-     * @return Task
-     */
-    public function getTask()
+    public function getTask(): ?Task
     {
         return $this->task;
     }
 
-    /**
-     * @return bool
-     */
-    public function isValid()
+    public function isValid(): bool
     {
         return $this->task instanceof Task;
     }
