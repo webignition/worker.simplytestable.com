0e2cf06c8be36924ebb27fed9f47b4d314b87e72
Jon Cram <jon@webignition.net>
2019-03-27 16:43:48 +0000

    Update AbstractPostRequestFactory

---
+++ src/Services/Request/Factory/AbstractPostRequestFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Request\Factory;
+
+use App\Request\VerifyRequest;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+abstract class AbstractPostRequestFactory
+{
+    const PARAMETER_HOSTNAME = 'hostname';
+    const PARAMETER_TOKEN = 'token';
+
+    /**
+     * @var ParameterBag
+     */
+    protected $requestParameters;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $currentRequest = $requestStack->getCurrentRequest();
+        $this->requestParameters = $currentRequest
+            ? $currentRequest->request
+            : new ParameterBag();
+    }
+}
